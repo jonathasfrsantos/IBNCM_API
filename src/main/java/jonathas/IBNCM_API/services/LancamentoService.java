c9ac23ab37d41850be07799b6e6457633ca422ad
jonathasfrsantos <jonathasfrsantos@gmail.com>
2022-11-19 19:29:16 -0300

    Service lançamento CRUD

--- src/main/java/jonathas/IBNCM_API/services/LancamentoService.java
+++ src/main/java/jonathas/IBNCM_API/services/LancamentoService.java
@@ -1,12 +1,18 @@
 package jonathas.IBNCM_API.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import jakarta.persistence.EntityNotFoundException;
 import jonathas.IBNCM_API.entities.Lancamento;
 import jonathas.IBNCM_API.repositories.LancamentoRepository;
+import jonathas.IBNCM_API.services.exceptions.DataBaseException;
+import jonathas.IBNCM_API.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class LancamentoService {
@@ -16,6 +22,50 @@ public class LancamentoService {
 	
 	public List<Lancamento> findAll(){
 		return repository.findAll();
+		
+	}
+	
+	public Lancamento findById(Long id) {
+		Optional<Lancamento> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Lancamento insert(Lancamento obj) {
+		return repository.save(obj);
+	}
+	
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);	
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
+	}
+	
+	public Lancamento update(Long id, Lancamento obj) {
+		try {
+			Lancamento entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
+	}
+
+	private void updateData(Lancamento entity, Lancamento obj) {
+		entity.setData(obj.getData());
+		entity.setEntrada(obj.getEntrada());
+		entity.setSaida(obj.getSaida());
+		entity.setFinalidade(obj.getFinalidade());
+		entity.setHistorico(obj.getHistorico());
+		entity.setBancoCaixa(obj.getBancoCaixa());
 		
 	}
 	
